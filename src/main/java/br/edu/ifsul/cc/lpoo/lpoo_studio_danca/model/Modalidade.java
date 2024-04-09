605bfc86a8afb5d5ef4a98c8e87dc9786a575d22
Bernardo Damiani <bmdamiani17@gmail.com>
2024-04-09 10:07:57 -0300

    first commit

---
+++ src/main/java/br/edu/ifsul/cc/lpoo/lpoo_studio_danca/model/Modalidade.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsul.cc.lpoo.lpoo_studio_danca.model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author damen
+ */
+@Entity
+public class Modalidade implements Serializable {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+    private String descricao;
+
+    public Modalidade() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+    
+}
